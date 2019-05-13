ccd34ad1e615ecb83401b3f7b9f419c7672a55a3
Panepo <whisper.ko@gmail.com>
2019-05-13 14:54:13 +0800

    Add analytics
    Add google analytics

--- src/index.jsx
+++ src/index.jsx
@@ -4,9 +4,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import ReduxRoot from './ReduxRoot'
 import registerServiceWorker from './serviceWorker'
+import * as ReactGA from 'react-ga'
 
 const rootEl = document.getElementById('uzuki')
 ReactDOM.render(<ReduxRoot />, rootEl)
+
+ReactGA.initialize('UA-106126363-3')
+ReactGA.pageview(window.location.pathname + window.location.search)
 
 if (module.hot) {
   module.hot.accept('./ReduxRoot', () => {
