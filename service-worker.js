35118c0ec6bc6f317aa6324431caa7cb6ae1e4ae
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-24 14:58:14 +0800

    Deploy Panepo/Uzuki to github.com/Panepo/Uzuki.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Uzuki/precache-manifest.16da8c1464cfa9828ef92abf3ce8de05.js"
+  "/Uzuki/precache-manifest.883cdfdc761703f1db91e969a08c4ae4.js"
 );
 
 workbox.clientsClaim();
